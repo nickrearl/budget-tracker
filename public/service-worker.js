4da860dfd5f9133738d7c6dd0f57f63d34194d7a
Nick Earl <nick.r.earl@gmail.com>
2022-08-14 20:44:20 -0400

    updated service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,25 +1,14 @@
-/*
-  INFO: Your data needs a place to live when there is no Internet connection. That's what 
-  the cache is for. There is the general cache for images and such, and a data cache for 
-  data-specific stuff. I would just follow the naming conventions you see here. Note the 
-  versioning on each cache name. This is important.
-*/
 const APP_PREFIX = 'my-site-cache-';  
 const VERSION = 'v1';
 const CACHE_NAME = APP_PREFIX + VERSION;
 const DATA_CACHE_NAME = "data-cache-" + VERSION;
 
-/*
-  INFO: We need to provide an array of all urls that our PWA should cache (or basically make 
-  local copies of). In other words, we're telling the PWA to be prepared to use the service 
-  worker anytime the browser tries to hit any of these routes. In a large web app there could 
-  be lots of entries here.
-*/
 const FILES_TO_CACHE = [
   "/",
   "./index.html",
   "./css/styles.css",
-  // TODO: add the two files in the js directory
+  "./js/idb.js",
+  "./js/index.js",
   "./manifest.json",
   "./icons/icon-72x72.png",
   "./icons/icon-96x96.png",
@@ -31,11 +20,7 @@ const FILES_TO_CACHE = [
   "./icons/icon-512x512.png"
 ];
 
-/*
-  INFO: This code, as you might imagine, fires when the user has chosen to install the web app on 
-  their machine as a standalone PWA. You won't need to modify this code. Keep it exactly as-is. Notice 
-  the rather cryptic syntax. Yet another reason why PWA coding is evil.
-*/
+
 self.addEventListener("install", function(event) {
   event.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
@@ -46,18 +31,6 @@ self.addEventListener("install", function(event) {
 });
 
 
-/*
-  INFO: This is the heart of the PWA functionality. This code tells the service worker to listen for any events 
-  where a call is made to our server api. Remember that fetch is a utility built into every browser that allows 
-  it to make server calls. So we are listening for when fetch is being used. You can use all of this code 
-  below as-is. Again, more cryptic code.
-
-  Notice that if the route being requested (line 64) includes "/api/" we are assuming that it's an api call. 
-  In other words, it's a request for data. This is the kind of thing that would fail if there's no Internet 
-  connection. So, if the Internet *is* working, we tell the service worker to make a cached copy of any 
-  data that comes back from that request. If the Internet *isn't* working, we tell the service worker to 
-  look up that cached copy and use that instead.
-*/
 self.addEventListener("fetch", function(event) {
   // cache all get requests to /api routes
   if (event.request.url.includes("/api/")) {
@@ -82,10 +55,7 @@ self.addEventListener("fetch", function(event) {
     return;
   }
 
-/*
-  INFO: This code is triggered if the request is NOT an api request. In the case of this app, that means 
-  it's a request for the home page, so we tell the service worker to grab its local copy of the home page.
-*/
+
   event.respondWith(
     fetch(event.request).catch(function() {
       return caches.match(event.request).then(function(response) {
@@ -100,11 +70,7 @@ self.addEventListener("fetch", function(event) {
   );
 });
 
-/*
-  INFO: More cryptic code. Basically, whenever our service worker is activated, we want to clear out
-  anything it has in its cache and get a fresh new cache set up. This code looks like it was written 
-  by someone with a background in Medieval Torture.
-*/
+
 self.addEventListener('activate', function (e) {
   e.waitUntil(
     caches.keys().then(function (keyList) {
